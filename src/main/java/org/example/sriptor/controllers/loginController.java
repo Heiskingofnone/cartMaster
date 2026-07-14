e4bae2f7f779357c5306ad8d63dc6de88a3c43ec
Abass Bakie <abassbakiemawuli99@gmail.com>
2026-07-14 23:23:08 +0000

    ...

--- src/main/java/org/example/sriptor/controllers/loginController.java
+++ src/main/java/org/example/sriptor/controllers/loginController.java
@@ -1,5 +1,18 @@
 package org.example.sriptor.controllers;
 
-public class loginController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
+//Controls verification at the front end level. Takes input from form the form and stores it in variables for later use for confirmation
+public class loginController {
+    @FXML
+    private TextField loginUsername;// an instance of a textfield is acknowledged
+    @FXML //Connects loginController + fxml in order to acknowledge object instance
+    private TextField loginPassword;//an instance of a textfield is acknowledged
+
+    private void onSubmit(ActionEvent event){
+        String username = loginUsername.getText();
+        String password = loginPassword.getText();
+    }
 }
