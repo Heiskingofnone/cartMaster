88fc409f230e27fd10caf63600ae7bd9bf483397
Abass Bakie <abassbakiemawuli99@gmail.com>
2026-09-18 23:09:30 +0000

    refactor: refactored object models and updated to suit database requirements id from int to text

--- src/main/java/org/example/sriptor/models/organizations.java
+++ src/main/java/org/example/sriptor/models/organizations.java
@@ -1,9 +1,18 @@
 package org.example.sriptor.models;
 
 public class organizations {
-    public String companyName;
-    public String countryName;
-    public String cityName;
+    private String companyId;
+    private String companyName;
+    private String ownerId;
+    private String countryName;
+    private Industry industry;
+    private String subscriptionPlan;
+    private String subscriptionStatus;
+    private String maxStoresAllowed;
+    private int isActive;
+    private String createdAt;
+    private String updatedAt;
+
     public enum Industry{
         PHARMACY("Pharmacy & Healthcare"),
         COSMETIC("Cosmetics & Beauty Supply"),
@@ -21,4 +30,53 @@ public class organizations {
         Industry(String displayName){this.displayName = displayName;}
         public String toString(){return this.displayName;}
     }
+
+
+    public organizations(String companyId, String companyName, String ownerId, String countryName, Industry industry, String subscriptionPlan, String subscriptionStatus, String maxStoresAllowed, int isActive, String createdAt, String updatedAt){
+        this.companyId = companyId;
+        this.companyName = companyName;
+        this.ownerId = ownerId;
+        this.countryName = countryName;
+        this.industry = industry;
+        this.subscriptionPlan = subscriptionPlan;
+        this.subscriptionStatus = subscriptionStatus;
+        this.maxStoresAllowed = maxStoresAllowed;
+        this.isActive = isActive;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+    public organizations(String companyId, String companyName, String ownerId, String countryName, Industry industry){
+        this.companyId = companyId;
+        this.companyName = companyName;
+        this.ownerId = ownerId;
+        this.countryName = countryName;
+        this.industry = industry;
+    }
+
+    //Getters
+    public String getCompanyId(){return companyId;}
+    public String getCompanyName(){return companyName;}
+    public String getOwnerId() {return ownerId;}
+    public String getCountryName(){return countryName;}
+    public Industry getIndustry() {return industry;}
+    public String getSubscriptionPlan(){return subscriptionPlan;}
+    public String getSubscriptionStatus(){return subscriptionStatus;}
+    public String getMaxStoresAllowed(){return maxStoresAllowed;}
+    public int getIsActive() {return isActive;}
+    public String getCreatedAt() {return createdAt;}
+    public String getUpdatedAt() {return updatedAt;}
+
+
+    //Setter
+    public void setCompanyId(String companyId) {this.companyId = companyId;}
+    public void setCompanyName(String companyName) {this.companyName = companyName;}
+    public void setOwnerId(String ownerId) {this.ownerId = ownerId;}
+    public void setCountryName(String countryName) {this.countryName = countryName;}
+    public void setIndustry(Industry industry) {this.industry = industry;}
+    public void setSubscriptionPlan(String subscriptionPlan) {this.subscriptionPlan = subscriptionPlan;}
+    public void setSubscriptionStatus(String subscriptionStatus) {this.subscriptionStatus = subscriptionStatus;}
+    public void setMaxStoresAllowed(String maxStoresAllowed) {this.maxStoresAllowed = maxStoresAllowed;}
+    public void setIsActive(int isActive) {this.isActive = isActive;}
+    public void setCreatedAt(String createdAt) {this.createdAt = createdAt;}
+    public void setUpdatedAt(String updatedAt) {this.updatedAt = updatedAt;}
 }
--- src/main/java/org/example/sriptor/models/user.java
+++ src/main/java/org/example/sriptor/models/user.java
@@ -10,22 +10,21 @@ public class user {
     private Boolean isActive;
     private String createdDate;
     private String updatedDate;
+    private CountryCodes countryCodes;
 
-
+    //enums
     public enum Role{
         CASHIER("Cashier"),
         ADMIN("Administrator");
-        private  final String displayName;
 
+        private  final String displayName;
         Role(String displayName){
             this.displayName = displayName;
         }
-
         public String toString(){
             return displayName;
         }
     }
-
     public enum CountryCodes{
         GHANA("+233"),
         Nigeria("+234"),
@@ -36,6 +35,8 @@ public class user {
         public String toString() {return displayName;}
     }
 
+
+    //Constructors
     public user(String firstName, String lastName, String email, String password, Role role, String phoneNumber){
         this.firstName = firstName;
         this.lastName = lastName;
@@ -44,74 +45,59 @@ public class user {
         this.role = role;
         this.phoneNumber = phoneNumber;
     }
-    //setters
 
+
+    //setters
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public void setRole(Role role) {
         this.role = role;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public void setIsActive(Boolean isActive) {
         this.isActive = isActive;
     }
-
     public void setCreatedDate(String createdDate) {
         this.createdDate = createdDate;
     }
-
     public void setUpdatedDate(String updatedDate) {
         this.updatedDate = updatedDate;
     }
-    
     public void setPhoneNumber(String phoneNumber){this.phoneNumber =phoneNumber;}
-    //getters
 
+    
+    //getters
     public String getFirstName() {
         return firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public String getEmail() {
         return email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public Role getRole() {
         return role;
     }
-
     public Boolean getIsActive() {
         return isActive;
     }
-
     public String getCreatedDate() {
         return createdDate;
     }
-
     public String getPhoneNumber(){return phoneNumber;}
-
-    public String getUpdatedDate() {
-        return updatedDate;
+    public String getUpdatedDate() {return updatedDate;}
     }
-}
+
